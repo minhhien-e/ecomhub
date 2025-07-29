dbb2187ed4746f9ec0b41dabca0ea77a27d070e1
minhhien-e <203855345+minhhien-e@users.noreply.github.com>
2025-07-29 14:34:27 +0700

    feat(permission): add unique key column to permission table

--- auth-service/adapter/src/main/resources/db/migration/V6__add_permissionkey.sql
+++ auth-service/adapter/src/main/resources/db/migration/V6__add_permissionkey.sql
@@ -2,9 +2,4 @@ ALTER TABLE permission
     ADD `key` VARCHAR(100) NULL;
 
 ALTER TABLE permission
-    MODIFY `key` VARCHAR (100) NOT NULL;
-
-ALTER TABLE permission
     ADD CONSTRAINT uc_permission_key UNIQUE (`key`);
-
-CREATE INDEX idx_permission_key ON permission (`key`);
