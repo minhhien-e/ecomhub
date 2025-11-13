d4a0b57761e1a34fc427a5bc457326ec08f19cd2
minhhien-e <203855345+minhhien-e@users.noreply.github.com>
2025-11-13 15:51:14 +0700

    feat(architecture): create Clean Architecture hierarchy for UserService

---
+++ user-service/api/src/main/java/ecomhub/userservice/api/boostrap/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package ecomhub.userservice.api.boostrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
---
+++ user-service/api/src/test/java/ecomhub/userservice/application/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ecomhub.userservice.application.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
