d21afe62887405f58cf3895f65d77fd700b26175
LYTDUNG <22130054@st.hcmuaf.edu.vn>
2025-12-16 22:48:56 +0700

    refactor: chuẩn hóa ApiResponse với errorCode kiểu Integer (null khi thành công) và phạm vi 600-699

--- src/main/java/ecomhub/PromotionDiscountService/exception/PromotionValidationException.java
+++ src/main/java/ecomhub/PromotionDiscountService/exception/PromotionValidationException.java
@@ -1,7 +1,18 @@
 package ecomhub.PromotionDiscountService.exception;
 
+import lombok.Getter;
+
+@Getter
 public class PromotionValidationException extends RuntimeException {
+
+    private final int errorCode;
     public PromotionValidationException(String message) {
-        super(message);
+        this(message, 602);
     }
+
+    public PromotionValidationException(String message, int errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
 }
